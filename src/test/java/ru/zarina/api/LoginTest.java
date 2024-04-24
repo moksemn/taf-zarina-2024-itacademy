c786dd98b6d8176bc13b5557f6e252a2029b6845
Maksim Novikov <moksemnovikov@gmail.com>
2024-04-24 11:58:37 +0300

    api

--- src/test/java/ru/zarina/api/LoginTest.java
+++ src/test/java/ru/zarina/api/LoginTest.java
@@ -1,114 +1,86 @@
 package ru.zarina.api;
 
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import ru.zarina.api.factory.LoginFactory;
 import ru.zarina.api.service.LoginService;
 import ru.zarina.listener.ApiTestListener;
-import ru.zarina.util.Decoder;
-
-import static io.restassured.RestAssured.given;
 
 @ExtendWith(ApiTestListener.class)
 public class LoginTest {
     @Test
     @DisplayName("API:POST Unregistered user login")
     public void testUnregisteredUserLogin() {
-        Response response = given()
-                .body(LoginService.getBodyWithUnregisteredUser())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(404, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_INVALID_DATA, Decoder.decodedJsonResponseBody(response));
-
+        ValidatableResponse response = LoginService.
+                post(LoginFactory.getBodyWithUnregisteredUser());
+        Assertions.assertEquals(404, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_INVALID_DATA, LoginService.getBody(response));
     }
 
     @Test
     @DisplayName("API:POST Registered user login ")
     public void testRegisteredUserLogin() {
-        Response response = given()
-                .body(LoginService.getBodyWithRegisteredUser())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(200, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_USER_ID, response.jsonPath().getString("user.id"));
+        ValidatableResponse response = LoginService.
+                post(LoginFactory.getBodyWithRegisteredUser());
+        Assertions.assertEquals(200, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_USER_ID, LoginService.getUserId(response));
     }
 
     @Test
     @DisplayName("API:POST Login with empty password field")
     public void testLoginEmptyPasswordField() {
-        Response response = given()
-                .body(LoginService.getBodyWithEmptyPassword())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(422, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_EMPTY_PASSWORD, Decoder.decodedJsonResponseBody(response));
+        ValidatableResponse response = LoginService
+                .post(LoginFactory.getBodyWithEmptyPassword());
+        Assertions.assertEquals(422, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_EMPTY_PASSWORD, LoginService.getBody(response));
     }
 
     @Test
     @DisplayName("API:POST Login with empty email field")
     public void testLoginEmptyEmailField() {
-        Response response = given()
-                .body(LoginService.getBodyWithEmptyEmail())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(422, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_EMPTY_EMAIL, Decoder.decodedJsonResponseBody(response));
+        ValidatableResponse response = LoginService
+                .post(LoginFactory.getBodyWithEmptyEmail());
+        Assertions.assertEquals(422, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_EMPTY_EMAIL, LoginService.getBody(response));
     }
 
     @Test
     @DisplayName("API:POST Login with empty email and password fields")
     public void testLoginEmptyEmailAndPasswordFields() {
-        Response response = given()
-                .body(LoginService.getBodyWithEmptyEmailAndPassword())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(422, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_EMPTY_DATA, Decoder.decodedJsonResponseBody(response));
+        ValidatableResponse response = LoginService
+                .post(LoginFactory.getBodyWithEmptyEmailAndPassword());
+        Assertions.assertEquals(422, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_EMPTY_DATA, LoginService.getBody(response));
     }
-
 
     @Test
     @DisplayName("API:POST Login with invalid email format")
     public void testLoginInvalidEmailFormat() {
-        Response response = given()
-                .body(LoginService.getBodyWithInvalidEmailFormat())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(422, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_INVALID_EMAIL_FORMAT, Decoder.decodedJsonResponseBody(response));
+        ValidatableResponse response = LoginService
+                .post(LoginFactory.getBodyWithInvalidEmailFormat());
+        Assertions.assertEquals(422, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_INVALID_EMAIL_FORMAT, LoginService.getBody(response));
     }
 
     @Test
     @DisplayName("API:POST Login with invalid password format")
     public void testLoginInvalidPasswordFormat() {
-        Response response = given()
-                .body(LoginService.getBodyWithInvalidPasswordFormat())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(404, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_INVALID_DATA, Decoder.decodedJsonResponseBody(response));
+        ValidatableResponse response = LoginService
+                .post(LoginFactory.getBodyWithInvalidPasswordFormat());
+        Assertions.assertEquals(404, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_INVALID_DATA, LoginService.getBody(response));
     }
 
     @Test
     @DisplayName("API:POST Login with invalid email and password format")
     public void testLoginInvalidEmailAndPasswordFormat() {
-        Response response = given()
-                .body(LoginService.getBodyWithInvalidEmailAndPasswordFormat())
-                .headers(LoginService.getHeaders())
-                .when()
-                .post(LoginService.URL);
-        Assertions.assertEquals(422, response.getStatusCode());
-        Assertions.assertEquals(LoginService.EXPECTED_BODY_INVALID_EMAIL_FORMAT, Decoder.decodedJsonResponseBody(response));
+        ValidatableResponse response = LoginService
+                .post(LoginFactory.getBodyWithInvalidEmailAndPasswordFormat());
+        Assertions.assertEquals(422, LoginService.getStatusCode(response));
+        Assertions.assertEquals(LoginFactory.EXPECTED_BODY_INVALID_EMAIL_FORMAT, LoginService.getBody(response));
     }
 }
 
--- src/test/java/ru/zarina/api/SearchTest.java
+++ src/test/java/ru/zarina/api/SearchTest.java
@@ -1,38 +1,32 @@
 package ru.zarina.api;
 
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import ru.zarina.api.factory.SearchFactory;
 import ru.zarina.api.service.SearchService;
 import ru.zarina.listener.ApiTestListener;
-
-import static io.restassured.RestAssured.given;
 
 @ExtendWith(ApiTestListener.class)
 public class SearchTest {
     @Test
     @DisplayName("API:GET Search correct product")
     public void testSearchCorrectProduct() {
-        Response response = given()
-                .headers(SearchService.getHeaders())
-                .queryParams(SearchService.getQueryParamsWithCorrectProduct())
-                .when()
-                .get(SearchService.URL);
-        Assertions.assertEquals(200, response.getStatusCode());
-        Assertions.assertFalse(response.jsonPath().getBoolean("zeroQueries"));
+        ValidatableResponse response = SearchService
+                .get(SearchFactory.getQueryParamsWithCorrectProduct());
+        Assertions.assertEquals(200, SearchService.getStatusCode(response));
+        Assertions.assertFalse(SearchService.hasZeroQueries(response));
     }
+
 
     @Test
     @DisplayName("API:GET Search incorrect product")
     public void testSearchIncorrectProduct() {
-        Response response = given()
-                .headers(SearchService.getHeaders())
-                .queryParams(SearchService.getQueryParamsWithIncorrectProduct())
-                .when()
-                .get(SearchService.URL);
-        Assertions.assertEquals(200, response.getStatusCode());
-        Assertions.assertTrue(response.jsonPath().getBoolean("zeroQueries"));
+        ValidatableResponse response = SearchService
+                .get(SearchFactory.getQueryParamsWithIncorrectProduct());
+        Assertions.assertEquals(200, SearchService.getStatusCode(response));
+        Assertions.assertTrue(SearchService.hasZeroQueries(response));
     }
 }
